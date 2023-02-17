bdf2fd6d62451c648095233032de69379dd78c8a
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-17 23:42:12 +0530

    EmployeeData

--- EmployeeData/Program.cs
+++ EmployeeData/Program.cs
@@ -4,12 +4,21 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0, empWage = 0;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -18,7 +27,7 @@
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
         }
         }
     }
