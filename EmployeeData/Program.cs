fd63f8b7a78f004a8f0198a0bdfd436414b00062
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-17 23:54:09 +0530

    EmployeeData

--- EmployeeData/Program.cs
+++ EmployeeData/Program.cs
@@ -2,33 +2,40 @@
 {
     internal class Program
     {
+        
+        
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            //Computation
+
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
-        }
     }
+}
+
+
+
+
+
+
+
 
